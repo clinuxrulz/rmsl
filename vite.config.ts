e122adc9d43b8e2061d634cd2c414ff484507419
Clinton Selke <clinuxrulz@gmail.com>
2026-07-18 06:13:41 +1000

    build scripts

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/rmsl.ts',
+      name: 'rmsl',
+      fileName: 'rmsl',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
+  plugins: [
+    dts({
+      include: ['src/**/*.ts'],
+      outDir: 'dist',
+      rollupTypes: true,
+    }),
+  ],
+})
