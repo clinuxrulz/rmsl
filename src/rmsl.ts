fafaa593aacbad9d70ae9fc1f09158280a28951d
Clinton Selke <clinuxrulz@gmail.com>
2026-07-17 20:53:06 +1000

    allowed for zero arg vec2, vec3, vec4 which will construct a vector full of zeros

--- src/rmsl.ts
+++ src/rmsl.ts
@@ -498,7 +498,10 @@ export function float(v: number | Node<"int">): Node<"float"> {
   }
   return node({ _t: "float", type: "float", value: v }) as Node<"float">;
 }
-export function vec2(x: FloatLike | Node<"vec2"> | Node<"vec3"> | Node<"vec4">, y?: FloatLike): Node<"vec2"> {
+export function vec2(x?: FloatLike | Node<"vec2"> | Node<"vec3"> | Node<"vec4">, y?: FloatLike): Node<"vec2"> {
+  if (x === undefined) {
+    return node({ _t: "vec2", type: "construct", params: [wrapValue(0)] }) as Node<"vec2">;
+  }
   if (isNode(x)) {
     let params = [x as BaseNode<ShaderType>];
     if (y !== undefined) params.push(wrapValue(y) as BaseNode<ShaderType>);
@@ -512,7 +515,10 @@ export function vec2(x: FloatLike | Node<"vec2"> | Node<"vec3"> | Node<"vec4">, 
   }
   return node({ _t: "vec2", type: "construct", params: [wrapValue(x), y as BaseNode<ShaderType>] }) as Node<"vec2">;
 }
-export function vec3(x: FloatLike | Node<"vec3"> | Node<"vec4">, y?: FloatLike, z?: FloatLike): Node<"vec3"> {
+export function vec3(x?: FloatLike | Node<"vec3"> | Node<"vec4">, y?: FloatLike, z?: FloatLike): Node<"vec3"> {
+  if (x === undefined) {
+    return node({ _t: "vec3", type: "construct", params: [wrapValue(0)] }) as Node<"vec3">;
+  }
   if (isNode(x)) {
     let params = [x as BaseNode<ShaderType>];
     if (y !== undefined) params.push(wrapValue(y) as BaseNode<ShaderType>);
@@ -532,7 +538,10 @@ export function vec3(x: FloatLike | Node<"vec3"> | Node<"vec4">, y?: FloatLike, 
   if (z !== undefined) params.push(wrapValue(z) as BaseNode<ShaderType>);
   return node({ _t: "vec3", type: "construct", params }) as Node<"vec3">;
 }
-export function vec4(x: FloatLike | Node<"vec2"> | Node<"vec3"> | Node<"vec4">, y?: FloatLike, z?: FloatLike, w?: FloatLike): Node<"vec4"> {
+export function vec4(x?: FloatLike | Node<"vec2"> | Node<"vec3"> | Node<"vec4">, y?: FloatLike, z?: FloatLike, w?: FloatLike): Node<"vec4"> {
+  if (x === undefined) {
+    return node({ _t: "vec4", type: "construct", params: [wrapValue(0)] }) as Node<"vec4">;
+  }
   if (isNode(x)) {
     let params = [x as BaseNode<ShaderType>];
     if (y !== undefined) params.push(wrapValue(y) as BaseNode<ShaderType>);
